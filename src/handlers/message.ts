283e56b0812ceeb8003b9d04da0f55c2b97cf4ba
PGRjoystick <30430815+PGRjoystick@users.noreply.github.com>
2024-09-13 12:44:58 +0700

    add additional checks to handle potential issues

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -249,7 +249,7 @@ async function handleIncomingMessage(message: Message) {
 
 			// pkgcreate
 			if (startsWithIgnoreCase(messageString, '!pkgcreate')) {
-				const args = messageString.split(' ').slice(1);
+				const args = messageString.split(' ').slice(1).map(arg => arg.trim());;
 				if (args.length < 3) {
 					cli.print('Paket gagal dibuat karena format salah. command yang di input: ' + `"${messageString}"`);
 					message.reply('Format salah! Gunakan: !pkgcreate Nama_Paket Harga Key');
